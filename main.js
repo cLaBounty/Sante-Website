a0ed6f326dae3e2d2e84183a11a8b78f9504d4da
cLaBounty <camlabounty@gmail.com>
2024-01-02 20:08:02 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// TODO: update with accurate links
+const APP_STORE_URL = "https://cameronlabounty.com"
+const GOOGLE_PLAY_URL = "https://cameronlabounty.com"
+
+const onAppStoreDownloadClicked = () => {
+    window.open(APP_STORE_URL)
+}
+
+const onGooglePlayDownloadClicked = () => {
+    window.open(GOOGLE_PLAY_URL)
+}
